c692bcce90e3ff84621f4cec786c12cf3a73f005
voin57rus <88772553+voin57rus@users.noreply.github.com>
2024-10-02 18:00:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ let item = "";
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
-
 btn1.addEventListener("click", function(){
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
