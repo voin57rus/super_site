8897a116a7b92efa8fa4e4512c7277c365afe53e
voin57rus <88772553+voin57rus@users.noreply.github.com>
2024-10-28 23:11:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,96 @@
+let tg = window.Telegram.WebApp;
 
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+
+btn1.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вы выбрали товар 1");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+
+btn2.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Ссылканн на видео");
+        item = "https://youtu.be/S5IAs-wWKCA";
+        tg.MainButton.show();
+    }
+});
+
+btn3.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вы выбрали товар 3");
+        item = "3";
+        tg.MainButton.show();
+    }
+});
+
+btn4.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вы выбрали товар 4");
+        item = "4";
+        tg.MainButton.show();
+    }
+});
+
+btn5.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вы выбрали товар 5");
+        item = "5";
+        tg.MainButton.show();
+    }
+});
+
+btn6.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вы выбрали товар 6");
+        item = "6";
+        tg.MainButton.show();
+    }
+});
+
+// Открывает ссылку или отправляет данные при нажатии MainButton
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    if (item.startsWith("http")) {
+        window.open(item, "_blank");
+    } else {
+        tg.sendData(item);
+    }
+});
+
+// Отображение информации о пользователе
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+// Отображаем имя, фамилию и ID пользователя
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}
+ID: ${tg.initDataUnsafe.user.id}`;
+
+usercard.appendChild(p);
