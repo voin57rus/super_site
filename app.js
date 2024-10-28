5e5feac367f6954cc7b16f0be6cdba9a97c2cc43
voin57rus <88772553+voin57rus@users.noreply.github.com>
2024-10-28 22:52:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ btn1.addEventListener("click", function(){
         tg.MainButton.hide();
     }
     else {
-        tg.MainButton.setText("Вы выбрали товар 1");
+        tg.MainButton.setText("тест");
         item = "1";
         tg.MainButton.show();
     }
